686b01b57e0152212de88f8fae46af196f49855d
robbie10001/rn <robbie_c1000@hotmail.com>
2019-09-04 15:44:51 +1000

    Skeleton Draft

--- Terminal_Application/index.rb
+++ Terminal_Application/index.rb
@@ -15,9 +15,9 @@ answer1 = gets.chomp
     puts "you got it wrong!"
 end 
 puts 
+
 puts "#{name}, What year was Magna Carta?"
 puts "a. 1160, b. 1185, c. 1215, d. 1250"
-
 answer2 = gets.chomp 
  if answer2 == "c"
     puts "Bang!, that's right!"
@@ -25,10 +25,10 @@ answer2 = gets.chomp
     puts "Sorry #{name}, that's not it!"
  end 
 puts 
- puts "What year was the petition of rights?"
- puts "a. 864, b. 1420, c. 1710, d. 1628"
 
- answer3 = gets.chomp 
+puts "What year was the petition of rights?"
+puts "a. 864, b. 1420, c. 1710, d. 1628"
+answer3 = gets.chomp 
    if answer3 == "d"
     puts "Absoultely!"
    else
@@ -37,7 +37,6 @@ puts
 
 puts "In What year was the Glorious Revolution in England?"
 puts "a. 1232, b. 1542, c. 1628, d. 1723"
-
 answer4 = gets.chomp 
  if answer4 == "c"
     puts "You got it!"
@@ -45,9 +44,9 @@ answer4 = gets.chomp
     puts "You got it wrong, #{name}"
  end 
 puts 
+
 puts "In what year did the first Parliment of Great Britain take place?" 
 puts "a. 1707, b. 1743, c. 1794, d. 1832" 
-
 answer5 = gets.chomp 
  if answer5 == "a"
     puts "Yep, that's the one!"
@@ -55,53 +54,59 @@ answer5 = gets.chomp
     puts "Sorry, that ain't it"
  end 
  puts 
- puts "It what year did the American War of Independece Begin?"
- puts "a. 1655, b. 1715, c. 1776, d. 1832"
-
- answer6 = gets.chomp 
+ 
+puts "It what year did the American War of Independence Begin?"
+puts "a. 1655, b. 1715, c. 1776, d. 1832"
+answer6 = gets.chomp 
  if answer6 == "c"
     puts "Yes, Yes, Yes!"
  else 
     puts "No, so sorry!"
  end 
-puts 
+puts
+
 puts "In what year was the French Revolution?"
 puts "a. 1543, b. 1632, c. 1712, d. 1789"
-
 answer7 = gets.chomp 
 if answer7 == "d"
     puts "Liberty, equality, fraternity! You got it right!"
 else 
     puts "vive le roi!, You've got it wrong"
 end 
-puts 
+puts
+
 puts "In what year did the American Constitution take effect?"
 puts "a. 1770, b. 1783, c. 1789, d. 1812"
-
 answer8 = gets.chomp 
-if answer8 = "c"
+if answer8 == "c"
     puts "We the people!, say you got it right!"
 else 
     puts "That's wrong!"
 end 
 puts 
+
 puts "What year was the Great Reform Act?"
 puts "a. 1675, b. 1730, c. 1832, d. 1856" 
 answer9 = gets.chomp
-if answer9 = "c"
+if answer9 == "c"
     puts "You got it!"
 else 
     puts "Nope, sorry"
 end 
 puts 
+
 puts "In what year did New Zealand become the first country to introduce Universal Suffrage?"
 puts "a. 1843, b. 1856, c. 1875, d. 1893"
 answer10 = gets.chomp 
-if answer10 = "d"
+if answer10 == "d"
     puts "You're a superstar!"
 else 
-    puts "You got it wrong so sorry!"
+    puts "You got it wrong, so sorry!"
 end 
+puts 
+
+puts "Well done! #{name}! You've finished the quiz!"
+
 
 
 
