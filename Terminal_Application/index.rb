94588dc56ff94880e12b1afee446031db5aae157
robbie10001/rn <robbie_c1000@hotmail.com>
2019-09-04 14:12:14 +1000

    start of coding

---
+++ Terminal_Application/index.rb
@@ -0,0 +1,20 @@
+# First feature
+# feature name:
+
+
+puts "Welcome to the Democracy History Quiz, what is your name?"
+name = gets.chomp
+puts "Welcome #{name}, What is known as the birthplace of democracy?"
+puts "a. Paris, b. London, c. Rome, d. Athens"
+
+a, b, c = gets.chomp 
+puts "Sorry you are wrong"
+
+d= gets.chomp
+puts "You got it right"
+
+# Methods for Feature 2
+
+
+
+
