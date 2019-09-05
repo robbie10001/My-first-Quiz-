d5fa907a6afebe9ca81ecaf5b7ce0d62b75d20bc
robbie10001/rn <robbie_c1000@hotmail.com>
2019-09-05 11:13:40 +1000

    Added ability to answer in numerals

--- Terminal_Application/index.rb
+++ Terminal_Application/index.rb
@@ -5,14 +5,19 @@ require 'colorize'
 
 puts "Are you ready to play? yes or no".colorize(:color => :magenta,) 
 
-user_input = gets.chomp 
-while user_input == "no" 
-    puts "fair enough, buddy"
+while true 
+    user_input = gets.chomp 
+    if user_input == "no" 
+        puts "fair enough, buddy"
     exit! 
-    break 
+    break
+    end 
+
     if user_input == "yes" 
         puts "let's play the game"
-    end 
+        break 
+     end 
+     puts "Try Again!"
 end
 
 puts "Welcome to the Democracy History Quiz, what is your name?".colorize(:color => :magenta,)
@@ -21,8 +26,8 @@ puts "Hey #{name}, remember to answer the questions in lower case letters.".colo
 puts "What is known as the birthplace of democracy?".colorize(:color => :magenta,)
 puts "a. Paris, b. London, c. Rome, d. Athens"
 
-answer1 = gets.chomp 
-  if answer1 == "d"
+answer1 = gets.chomp.downcase 
+  if answer1 == "d" or 
     puts 
     puts "You got it, right!".colorize(:color => :green,)
     correct += 1
@@ -34,8 +39,8 @@ puts
 
 puts "#{name}, What year was Magna Carta?".colorize(:color => :magenta,)
 puts "a. 1160, b. 1185, c. 1215, d. 1250"
-answer2 = gets.chomp 
- if answer2 == "c"
+answer2 = gets.chomp.downcase 
+ if answer2 == "c" or answer2 == "1215" #or "1215"
     puts "Bang!, that's right!".colorize(:color => :green,)
     correct += 1
  else 
@@ -46,8 +51,8 @@ puts
 
 puts "What year was the petition of rights?".colorize(:color => :magenta,)
 puts "a. 864, b. 1420, c. 1710, d. 1628"
-answer3 = gets.chomp 
-   if answer3 == "d"
+answer3 = gets.chomp.downcase 
+   if answer3 == "d" or answer3 == "1628" #or "1628"
     puts "Absoultely!".colorize(:color => :green,)
     correct += 1
    else
@@ -57,8 +62,8 @@ answer3 = gets.chomp
 puts 
 puts "In What year was the Glorious Revolution in England?".colorize(:color => :magenta,)
 puts "a. 1232, b. 1542, c. 1628, d. 1723"
-answer4 = gets.chomp 
- if answer4 == "c"
+answer4 = gets.chomp.downcase 
+ if answer4 == "c" or answer4 == "1628" #"1628"
     puts "You got it!".colorize(:color => :green,)
     correct += 1
  else 
@@ -69,8 +74,8 @@ puts
 
 puts "In what year did the first Parliment of Great Britain take place?".colorize(:color => :magenta,) 
 puts "a. 1707, b. 1743, c. 1794, d. 1832" 
-answer5 = gets.chomp 
- if answer5 == "a"
+answer5 = gets.chomp.downcase 
+ if answer5 == "a" or answer5 == "1707" #"1707"
     puts "Yep, that's the one!".colorize(:color => :green,)
     correct += 1
  else 
@@ -81,8 +86,8 @@ answer5 = gets.chomp
  
 puts "It what year did the American War of Independence Begin?".colorize(:color => :magenta,)
 puts "a. 1655, b. 1715, c. 1776, d. 1832"
-answer6 = gets.chomp 
- if answer6 == "c"
+answer6 = gets.chomp.downcase 
+ if answer6 == "c" or answer6 == "1776" #"1776"
     puts "Yes, Yes, Yes!".colorize(:color => :green,)
     correct += 1
  else 
@@ -93,8 +98,8 @@ puts
 
 puts "In what year was the French Revolution?".colorize(:color => :magenta,)
 puts "a. 1543, b. 1632, c. 1712, d. 1789"
-answer7 = gets.chomp 
-if answer7 == "d"
+answer7 = gets.chomp.downcase 
+if answer7 == "d" or answer7 == "1789" #"1789"
     puts "Liberty, equality, fraternity! You got it right!".colorize(:color => :green,)
     correct += 1
 else 
@@ -105,8 +110,8 @@ puts
 
 puts "In what year did the American Constitution take effect?".colorize(:color => :magenta,)
 puts "a. 1770, b. 1783, c. 1789, d. 1812"
-answer8 = gets.chomp 
-if answer8 == "c"
+answer8 = gets.chomp.downcase 
+if answer8 == "c" or answer8 == "1789" #"1789"
     puts "We the people!, say you got it right!".colorize(:color => :green,)
     correct += 1
 else 
@@ -117,8 +122,8 @@ puts
 
 puts "What year was the Great Reform Act?".colorize(:color => :magenta,)
 puts "a. 1675, b. 1730, c. 1832, d. 1856" 
-answer9 = gets.chomp
-if answer9 == "c"
+answer9 = gets.chomp.downcase 
+if answer9 == "c" or answer9 == "1832" #"1832"
     puts "You got it!".colorize(:color => :green,)
     correct += 1
 else 
@@ -129,8 +134,8 @@ puts
 
 puts "In what year did New Zealand become the first country to introduce Universal Suffrage?".colorize(:color => :magenta,)
 puts "a. 1843, b. 1856, c. 1875, d. 1893"
-answer10 = gets.chomp 
-if answer10 == "d"
+answer10 = gets.chomp.downcase  
+if answer10 == "d" or answer10 == "1893"
     puts "You're a superstar!".colorize(:color => :green,)
     correct += 1
 else 
